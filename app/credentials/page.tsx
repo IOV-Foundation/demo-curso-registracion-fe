bd2f1b033f53d43fba28be8d524331d39361b5b9
Noel Flores <noelfloresr@gmail.com>
2024-03-27 07:44:31 -0400

    improving loading page

--- app/credentials/page.tsx
+++ app/credentials/page.tsx
@@ -7,7 +7,7 @@ import { getCredential } from '../lib/data';
 export default async function CredentialsPage() {
   const { name, company, role, email } = await getCredential();
   return (
-    <main className="mx-auto w-full max-w-screen-lg p-8">
+    <main className="mx-auto w-full max-w-screen-xl p-8">
       <div className="flex flex-col rounded-lg bg-gray-100 p-8 shadow">
         <div className="flex items-center justify-between border-b pb-8">
           <h2 className="text-3xl">Representante de Miembro</h2>
